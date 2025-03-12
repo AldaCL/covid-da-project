83fb30df52dc1afaf578b0ef23fe50414d49ad5b
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2025-03-11 21:02:43 -0600

    Adding time

--- COVID/scripts/limpieza.py
+++ COVID/scripts/limpieza.py
@@ -2,11 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 import csv
+import time
+
+ini = time.time_ns()
 
 def load_data():
     print("Cargando datos")
     i = 0
-    with open("../data/220720COVID19MEXICO.csv", "r") as file:
+    with open("../data/220720COVID19MEXICO.csv", "r", encoding="utf-8") as file:
         data_reader = csv.reader(file)
         # Print metadata of first row
         metadata = next(data_reader)
@@ -21,5 +24,8 @@ def load_data():
 
 load_data()
 
+fin = time.time_ns()
+
+print(f"Tiempo de ejecución: {(fin-ini)/1e9} segundos")
 
 
